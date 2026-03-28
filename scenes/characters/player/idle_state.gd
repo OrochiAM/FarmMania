25d7ebc5e941d2334fb6d13aa8b1432841eaa369
Aleksa Milic <baba12milic@gmail.com>
2026-03-28 17:35:05 +0100

    House and Working Door

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -27,11 +27,11 @@ func _on_next_transitions() -> void:
 	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
 		transition.emit("Chopping")
 	
-	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
-		transition.emit("Tilling")
-	
-	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+	elif player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
 		transition.emit("Watering")
+		
+	elif player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("Tilling")
 
 func _on_enter() -> void:
 	pass
